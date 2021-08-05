1d3553b63ce61d4e59c43071ba53b960887bb536
Kuldeep Singh <snipereagle99@gmail.com>
2021-08-05 22:39:44 +0530

    final change

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def prediction():
     return render_template('webpage.html')
 # @app.route('/predicted')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
